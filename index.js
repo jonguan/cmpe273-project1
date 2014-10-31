974f3dd70c923eb4a33b2cfedd06f50a720da37c
Jon Guan <jon@scanadu.com>
2014-10-31 12:52:17 -0700

    index from lab2 to support echo of request body on post

--- index.js
+++ index.js
@@ -1,7 +1,89 @@
+//To use the HTTP server and client one must require('http').
 var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+/*The net module provides you with an asynchronous network wrapper. 
+It contains methods for creating both servers and clients (called streams). 
+You can include this module with require('net');
+*/
+// var net = require('net');
+var connect = require('connect');
+var app = connect();
+app.use(connect.json()); // Parse JSON request body into `request.body`
+app.use(connect.urlencoded()); // Parse form in request body into `request.body`
+app.use(connect.cookieParser()); // Parse cookies in the request headers into `request.cookies`
+app.use(connect.query()); // Parse query string into `request.query`
 
+app.use('/', main);
+
+function main(request, response, next) {
+	switch (request.method) {
+		case 'GET': get(request, response); break;
+		case 'POST': post(request, response); break;
+		case 'DELETE': del(request, response); break;
+		case 'PUT': put(request, response); break;
+	}
+};
+
+function get(request, response) {
+	var cookies = request.cookies;
+	console.log(cookies);
+	if ('session_id' in cookies) {
+		var sid = cookies['session_id'];
+		if ( login.isLoggedIn(sid) ) {
+			response.setHeader('Set-Cookie', 'session_id=' + sid);
+			response.end(login.hello(sid));	
+		} else {
+			response.end("Invalid session_id! Please login again\n");
+		}
+	} else {
+		response.end("Please login via HTTP POST\n");
+	}
+};
+
+function post(request, response) {
+	// Read 'name and email from the request.body'
+	var aname = request.body.name;
+	var aemail = request.body.email;
+	console.log(request.body)
+	
+
+	// Set new session id to the 'session_id' cookie in the response
+	response.writeHead(200, {'Content-Type': 'application/json'});
+	// response.write('{"hello":"world"}');
+	response.end(JSON.stringify(request.body));
+};
+
+function del(request, response) {
+	console.log("DELETE:: Logout from the server");
+ 	// TODO: remove session id via login.logout(xxx)
+ 	// No need to set session id in the response cookies since you just logged out!
+ 	var sess_id = request.cookies['session_id'];
+
+ 	if (login.isLoggedIn(sess_id)) {
+		login.logout(sess_id);
+ 		response.writeHead(200, {'Content-Type': 'text/plain'});
+  		response.end('Logged out from the server\n');
+ 	} else {
+ 		response.end('Invlaid session_id! please login again\n');
+ 	};
+ 	
+};
+
+function put(request, response) {
+	console.log("PUT:: Re-generate new session_id for the same user");
+
+	// TODO: refresh session id; similar to the post() function
+	var newSessionId = login.refresh(request.cookies['session_id']);
+	console.log("PUT:: new session id = " + newSessionId);
+	if (newSessionId != null) {
+		 response.writeHead(200, {'Content-Type': 'text/plain', 'Set-Cookie':'session_id='+newSessionId});
+		 response.end("Re-freshed session id\n");
+	} else {
+		response.writeHead(403, {'Content-Type': 'text/plain'});
+		response.end('session_id not found\n');
+	};
+	
+};
+
+app.listen(8000);
+
+console.log("Node.JS server running at 8000...");
---
+++ proxy.js
@@ -0,0 +1,38 @@
+/* Port forwarding
+ To forward from localhost:9001 => localhost:80
+
+$ node proxy.js 9001 80
+Or localhost:9001 => otherhost:80
+
+$ node proxy.js 9001 otherhost:80
+*/
+
+//To use the HTTP server and client one must require('http').
+var http = require('http');
+/*The net module provides you with an asynchronous network wrapper. 
+It contains methods for creating both servers and clients (called streams). 
+You can include this module with require('net');
+*/
+var net = require('net');
+
+// parse "80" and "localhost:80" or even "42mEANINg-life.com:80"
+var addrRegex = /^(([a-zA-Z\-\.0-9]+):)?(\d+)$/;
+
+var addr = {
+    from: addrRegex.exec(process.argv[2]),
+    to: addrRegex.exec(process.argv[3])
+};
+
+if (!addr.from || !addr.to) {
+    console.log('Usage: <from> <to>');
+    return;
+}
+
+net.createServer(function(from) {
+    var to = net.createConnection({
+        host: addr.to[2],
+        port: addr.to[3]
+    });
+    from.pipe(to);
+    to.pipe(from);
+}).listen(addr.from[3], addr.from[2]);
