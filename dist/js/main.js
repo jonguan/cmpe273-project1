dc7d093bb886a451b573290ec6a2e75e1269c07d
Tushar Karkera <tusharkarkera1610@gmail.com>
2014-12-06 04:01:50 -0800

    changes to UI

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,27 +1,35 @@
-$(document).on('submit', '#get-form', function(e) {
-        e.preventDefault();
-        $.ajax({
-            type: "GET",
-            url: $(this).attr('action'),
-            data: $(this).serialize(),
-            success: function(response, textStatus, XMLHttpRequest) {
-                //process all the data you want over here in this block with index and element
-                console.log(response);
-            }
-        });
-        return false;
-});
+$(document).ready(function(){
 
-$(document).on('submit', '#post-form', function(e) {
-        e.preventDefault();
-        $.ajax({
-            type: "POST",
-            url: $(this).attr('action'),
-            data: $(this).serialize(),
-            success: function(response, textStatus, XMLHttpRequest) {
-                //process all the data you want over here in this block with index and element
-                console.log(response);
-            }
-        });
-        return false;
-});
+  $('#get-button').on('click', function(e) {
+    if($("#url").val()!=''){
+    e.preventDefault();
+    $.ajax({
+      type: "GET",
+      url: $("#url").val(),
+      success: function(response) {
+        console.log(response);
+        var res = JSON.stringify(response, null, '\t');
+        $('#server-response').val(res);
+      }
+    });
+  }
+    return false;
+  });
+
+  $('#post-button').on('click', function(e) {
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: $("#url").val(),
+      data: $("#json-data").val(),
+      contentType: 'application/json',
+      datatype: 'text',
+      success: function(response) {
+        console.log(response);
+        var res = JSON.stringify(response, null, '\t');
+        $('#server-response').val(res);
+      }
+    });
+    return false;
+  });
+})
