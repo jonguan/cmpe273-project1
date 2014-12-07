993486ee377986e7c802ddb945a6ac50f659ad5e
Jon Guan <jon@scanadu.com>
2014-12-06 22:43:23 -0800

    add timeout option

--- routingTable.js
+++ routingTable.js
@@ -6,6 +6,7 @@ var http = require('http');
 var server = http.createServer(function(req, res) {
 
     var options = require('./options');
+    var latency = require('./latency');
     //var options =  { "foo.com": "http://google.com","www.facebook.com": "http://facebook.com",  "www.google.com": "http://google.com",  "www.google.co.in":"http://google.com", "bar.com": "http://www.google.com","tanishq.co.in":"tanishq.co.in","http://stackoverflow.com":"http://stackoverflow.com"};
     console.log(req.headers.host);
     console.log(options);
@@ -16,9 +17,19 @@ var server = http.createServer(function(req, res) {
             res.end(err.message);
         }
     );
-    proxy.web(req, res, {
-        target: options[req.headers.host]
-    });
+
+    var delay = latency[req.headers.host];
+    if (delay == null) {
+        delay = 0;
+    }
+    console.log("delay = " + delay);
+
+    setTimeout(function () {
+        proxy.web(req, res, {
+            target: options[req.headers.host]
+        });
+    }, 1000 * delay);
+
     delete require.cache[require.resolve('./options')]
 
 }).listen(8000);
