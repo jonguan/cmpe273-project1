2eb6fae3b9327fad87a37e17a9c82f46f0989225
Jon Guan <jon@scanadu.com>
2014-12-06 16:39:18 -0800

    minor code indentatins

--- routingTable.js
+++ routingTable.js
@@ -3,23 +3,23 @@ var httpProxy = require('http-proxy')
 var proxy = httpProxy.createProxy();
 var http = require('http');
 
-var server = http.createServer(function(req, res) { 
+var server = http.createServer(function(req, res) {
 
-	var options = require('./options');                                  
-	//var options =  { "foo.com": "http://google.com","www.facebook.com": "http://facebook.com",  "www.google.com": "http://google.com",  "www.google.co.in":"http://google.com", "bar.com": "http://www.google.com","tanishq.co.in":"tanishq.co.in","http://stackoverflow.com":"http://stackoverflow.com"};
-	console.log(req.headers.host);
-	console.log(options);
-	console.log(options[req.headers.host]);
-	//res.write(options);
-proxy.on('error', function(err) {
-console.log(err.message);
-res.end(err.message);
-	}
-);
- proxy.web(req, res, {
-   target: options[req.headers.host]
-  });
-delete require.cache[require.resolve('./options')]
+    var options = require('./options');
+    //var options =  { "foo.com": "http://google.com","www.facebook.com": "http://facebook.com",  "www.google.com": "http://google.com",  "www.google.co.in":"http://google.com", "bar.com": "http://www.google.com","tanishq.co.in":"tanishq.co.in","http://stackoverflow.com":"http://stackoverflow.com"};
+    console.log(req.headers.host);
+    console.log(options);
+    console.log(options[req.headers.host]);
+    //res.write(options);
+    proxy.on('error', function(err) {
+            console.log(err.message);
+            res.end(err.message);
+        }
+    );
+    proxy.web(req, res, {
+        target: options[req.headers.host]
+    });
+    delete require.cache[require.resolve('./options')]
 
 }).listen(8000);
 
