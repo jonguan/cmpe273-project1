8408db545cddd454cb97373a5d9fff6ebba1d8b3
Tushar Karkera <tusharkarkera1610@gmail.com>
2014-12-09 16:42:36 -0800

    added code for PUT and DELETE in proxy server UI

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,18 +1,18 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
   $('#get-button').on('click', function(e) {
-    if($("#url").val()!=''){
-    e.preventDefault();
-    $.ajax({
-      type: "GET",
-      url: $("#url").val(),
-      success: function(response) {
-        console.log(response);
-        var res = JSON.stringify(response, null, '\t');
-        $('#server-response').val(res);
-      }
-    });
-  }
+    if ($("#url").val() != '') {
+      e.preventDefault();
+      $.ajax({
+        type: "GET",
+        url: $("#url").val(),
+        success: function(response) {
+          console.log(response);
+          var res = JSON.stringify(response, null, '\t');
+          $('#server-response').val(res);
+        }
+      });
+    }
     return false;
   });
 
@@ -32,4 +32,38 @@ $(document).ready(function(){
     });
     return false;
   });
+
+  $('#put-button').on('click', function(e) {
+    e.preventDefault();
+    $.ajax({
+      type: "PUT",
+      url: $("#url").val(),
+      data: $("#put-textarea").val(),
+      contentType: 'application/json',
+      datatype: 'text',
+      success: function(response) {
+        console.log(response);
+        var res = JSON.stringify(response, null, '\t');
+        $('#server-response').val(res);
+      }
+    });
+    return false;
+  });
+
+  $('#delete-button').on('click', function(e) {
+    e.preventDefault();
+    $.ajax({
+      type: "DELETE",
+      url: $("#url").val(),
+      contentType: 'application/json',
+      datatype: 'text',
+      success: function(response) {
+        console.log(response);
+        var res = JSON.stringify(response, null, '\t');
+        $('#server-response').val(res);
+      }
+    });
+    return false;
+  });
+
 })
