48c0bf4951dc36f2c70e143fac685372add6ba84
ashikhariprasad <ashikhariprasad@gmail.com>
2014-12-10 16:12:55 -0800

    adding UI integration

--- LoadBalancing_Servers/serv3.js
+++ LoadBalancing_Servers/serv3.js
@@ -2,6 +2,9 @@
 var http = require('http');
 var util = require('util');
 var serv = http.createServer(function (req, res) {
+	for(var i=0;i<5000;i++){
+		var j=i;
+	}
 	var afterDate = new Date();
 	var afterTime = afterDate.getTime();
 	console.log("Time On Server "+afterTime);
@@ -19,4 +22,7 @@ var serv = http.createServer(function (req, res) {
 	res.end('Hi from Server at 8182\n');
 });
 serv.listen(8182);
+//	res.writeHead(200, {'Content-Type': 'text/plain', 'Access-Control-Allow-Origin':'*','access-control-expose-headers':'X-HTTP-request-id, x-http-processing-time'});
+//	res.end('Server 1\n');
+//}).listen(8182, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:8182/');
